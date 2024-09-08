0ebeaa535651612663b4f2ae92a68b7abe218556
natsinch <tksinch@gmail.com>
2024-09-08 12:17:51 +0700

    Create request.go

---
+++ backend/entity/request.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type Request struct {
+	gorm.Model
+	Category     string
+	First_Name   string
+	Last_Name    string
+	Email        string
+	Subject      string
+	Mobile_Phone string
+	Message      string
+
+	// MemberID ทำหน้าที่เป็น FK
+	MemberID *uint
+	Member   []Member `gorm:"foriegnKey:MemberID"`
+}
