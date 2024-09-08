6b8a90312f92d1e231f44557d65ddfe251d96093
natsinch <tksinch@gmail.com>
2024-09-08 12:32:42 +0700

    relationship_benefits_notification

--- backend/entity/benefits.go
+++ backend/entity/benefits.go
@@ -4,21 +4,26 @@ import "gorm.io/gorm"
 
 type Benefits struct {
 	gorm.Model
-	BenefitsName	string
-	FlyingFrom		string
-	GoingTo			string
-	PointRequired	uint
-	Quantity		uint
-	Code			string
-	Trip			string
-	Type			string
+	BenefitsName  string
+	FlyingFrom    string
+	GoingTo       string
+	PointRequired uint
+	Quantity      uint
+	Code          string
+	Trip          string
+	Type          string
 
+	// AirlineID ทำหน้าที่เป็น FK
 	AirlineID *uint
-	Airline Airline `gorm:"foreignKey:AirlineID"`
+	Airline   Airline `gorm:"foreignKey:AirlineID"`
 
+	// AdminID ทำหน้าที่เป็น FK
 	AdminID *uint
-	Admin Admin `gorm:"foreignKey:AdminID"`
+	Admin   Admin `gorm:"foreignKey:AdminID"`
 
 	// 1 benefit เป็นเจ้าของได้หนึ่ง payment
 	Payment Payment `gorm:"foreignKey:BenefitID"`
+
+	// 1 Benefit เป็นเจ้าของได้หลาย Notification
+	Notifications []Notification `gorm:"foreignKey:BenefitID"`
 }
