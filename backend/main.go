dbd378e551e4da48e9e702f7b8502d962798eca9
natsinch <tksinch@gmail.com>
2024-09-08 15:45:55 +0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -12,5 +12,5 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&entity.Airport{}, &entity.Admin{}, &entity.Airline{}, &entity.Flight{}, &entity.FlightDetails{}, &entity.FlightAndFlightDetails{}, &entity.TypeOfFlight{})
+	db.AutoMigrate(&entity.Airport{}, &entity.Admin{}, &entity.Airline{}, &entity.Flight{}, &entity.FlightDetails{}, &entity.FlightAndFlightDetails{}, &entity.TypeOfFlight{}, &entity.Member{}, &entity.Benefits{}, &entity.Request{}, &entity.Notification{}, &entity.Booking{}, &entity.Payment{})
 }
