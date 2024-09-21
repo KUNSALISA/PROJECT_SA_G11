0cdf31f14661b4cdb963faa9ab9c5828447ddd53
seemmer <seemlike24@gmail.com>
2024-09-21 14:22:19 +0700

    DELEAT seat

--- backend/entity/booking.go
+++ backend/entity/booking.go
@@ -15,7 +15,6 @@ type Booking struct {
 	PAYMENT Payment `gorm:"foreignKey:BookingID"`
 
 	Booking_passenger                 []Booking_passenger                 `gorm:"foreignKey:BookingID"`
-	Booking_seat                      []Booking_seat                      `gorm:"foreignKey:BookingID"`
 	Booking_baggage                   []Booking_baggage                   `gorm:"foreignKey:BookingID"`
 	Booking_food_beverage             []Booking_food_beverage             `gorm:"foreignKey:BookingID"`
 	Booking_flight_and_flight_details []Booking_flight_and_flight_details `gorm:"foreignKey:BookingID"`
